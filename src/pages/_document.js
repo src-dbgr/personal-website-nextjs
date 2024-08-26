195d611063c190503607c48d321ba79cb16831c4
src-dbgr <src-dbgr@gmail.com>
2024-08-26 22:46:31 +0200

    add more meta tags and extend manifest

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/icons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/icons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/icons/favicon-16x16.png" />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
