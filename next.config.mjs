56f58e895dddc2d32d7eb67f68f31a895916838d
src-dbgr <src.dbgr@gmail.com>
2024-09-02 02:04:14 +0200

    test recaptcha

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,39 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      { hostname: 'res.cloudinary.com' },
-    ],
+    remotePatterns: [{ hostname: "res.cloudinary.com" }],
     unoptimized: true,
   },
   env: {
     STRAPI_GRAPHQL_URL: process.env.STRAPI_GRAPHQL_URL,
     STRAPI_TOKEN: process.env.STRAPI_TOKEN,
-    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
   },
   async headers() {
-    const headers = [
+    return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: process.env.NODE_ENV === 'development' 
-              ? "default-src 'self'; script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com https://*.google-analytics.com https://res.cloudinary.com https://*.cloudinary.com; img-src 'self' data: https://www.google-analytics.com https://res.cloudinary.com; style-src 'self' 'unsafe-inline'; frame-ancestors 'none'; form-action 'self' https://formspree.io;"
-              : "default-src 'self'; script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com https://*.google-analytics.com https://res.cloudinary.com https://*.cloudinary.com; img-src 'self' data: https://www.google-analytics.com https://res.cloudinary.com; style-src 'self' 'unsafe-inline'; frame-ancestors 'none'; form-action 'self' https://formspree.io;"
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
           },
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
+            key: "Referrer-Policy",
+            value: "no-referrer-when-downgrade",
           },
           {
-            key: 'Referrer-Policy',
-            value: 'no-referrer-when-downgrade',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
+            key: "X-Frame-Options",
+            value: "DENY",
           },
         ],
       },
     ];
-
-    return headers;
   },
 };
 
